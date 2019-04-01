75bfb83d4fbb985837bff345c5d14331fc118c7e
Marina Kovaleva <marinakovaleva0@gmail.com>
2019-04-01 15:07:17 +0100

    initial commit

---
+++ class.h
@@ -0,0 +1,9 @@
+//
+// Created by Marina Kovaleva on 2019-04-01.
+//
+
+#ifndef PROJECT_CLASS_H
+#define PROJECT_CLASS_H
+
+
+#endif //PROJECT_CLASS_H
---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Marina Kovaleva on 2019-04-01.
+//
+
+#include "class.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
+
